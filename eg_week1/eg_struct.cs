85c4ac3b7e4b3b7dd4d21b64c21062bfedfcde7a
Thuan Pham <phamtanthuan2311@gmail.com>
2021-10-11 14:43:33 +0700

    💄style: clean code

--- eg_week1/eg_struct.cs
+++ eg_week1/eg_struct.cs
@@ -4,37 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
-{
-    struct PHANSO
-    {
-        int tuso, mauso;
-        public void nhapPhanSo()
-        {
-            Console.Write("\nNhap tu so =");
-            tuso = Convert.ToInt32( Console.ReadLine());
-            Console.Write("\nNhap mau so =");
-            mauso = Convert.ToInt32(Console.ReadLine());
-        }
-        public void xuatPhanSo()
-        {
-            Console.Write(tuso + "/" + mauso);
-        }
+namespace ConsoleApp1 {
+  struct PHANSO {
+    int tuso, mauso;
+    public void nhapPhanSo() {
+      Console.Write("\nNhap tu so =");
+      tuso = Convert.ToInt32(Console.ReadLine());
+      Console.Write("\nNhap mau so =");
+      mauso = Convert.ToInt32(Console.ReadLine());
     }
-
-    class Program
-    {
-
-        static void Main(string[] args)
-        {
-            PHANSO ps = new PHANSO();
-            ps.nhapPhanSo();
-            ps.xuatPhanSo();
-            PHANSO ps1 = new PHANSO();
-            ps1.nhapPhanSo();
-            ps1.xuatPhanSo();
-
-            Console.ReadLine();
-        }
+    public void xuatPhanSo() {
+      Console.Write(tuso + "/" + mauso);
     }
+  }
+
+  class Program {
+
+    static void Main(string[] args) {
+      PHANSO ps = new PHANSO();
+      ps.nhapPhanSo();
+      ps.xuatPhanSo();
+      PHANSO ps1 = new PHANSO();
+      ps1.nhapPhanSo();
+      ps1.xuatPhanSo();
+
+      Console.ReadLine();
+    }
+  }
 }
--- nhap_xuat_mang/Program.cs
+++ nhap_xuat_mang/Program.cs
@@ -1,38 +1,31 @@
 ﻿using System;
 
-namespace nhap_xuat_mang
-{
-    class Program
-    {
-        public static void nhap_mang(int[] a, int n)
-        {
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write("nhập giá trị phần tử thứ " + i + ": ");
-                a[i] = Convert.ToInt32(Console.ReadLine());
-            }
-        }
-
-        public static void xuat_mang(int[] a, int n)
-        {
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write(a[i] + "\t");
-            }
-        }
-
-        static void Main(string[] args)
-        {
-            int[] a;
-            int n;
-
-            Console.Write("Nhập n = ");
-            n = Convert.ToInt32(Console.ReadLine());
-            a = new int[n];
-
-            nhap_mang(a, n);
-
-            xuat_mang(a, n);
-        }
+namespace nhap_xuat_mang {
+  class Program {
+    public static void nhap_mang(int[] a, int n) {
+      for (int i = 0; i < n; i++) {
+        Console.Write("nhập giá trị phần tử thứ " + i + ": ");
+        a[i] = Convert.ToInt32(Console.ReadLine());
+      }
     }
+
+    public static void xuat_mang(int[] a, int n) {
+      for (int i = 0; i < n; i++) {
+        Console.Write(a[i] + "\t");
+      }
+    }
+
+    static void Main(string[] args) {
+      int[] a;
+      int n;
+
+      Console.Write("Nhập n = ");
+      n = Convert.ToInt32(Console.ReadLine());
+      a = new int[n];
+
+      nhap_mang(a, n);
+
+      xuat_mang(a, n);
+    }
+  }
 }
